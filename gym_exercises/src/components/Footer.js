0ff91b194b2e58752cac69b59693ecb01d5caa62
Talal <talalfarrukh200@gmail.com>
2022-08-18 22:25:40 +0500

    Design finished

--- gym_exercises/src/components/Footer.js
+++ gym_exercises/src/components/Footer.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+
+import Logo from '../assets/images/Logo-1.png'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box mt='80px' bgcolor='#fff3f4'>
+      <Stack gap='40px' alignItems='center' px='40x' pt='24px'>
+        <img src={Logo} width='200px' heigth='40px' />
+        <Typography varaint='h5' pb='40px' mt='20px'>Made with love by Talal Farrukh</Typography>
+      </Stack>
+    </Box>
   )
 }
 
